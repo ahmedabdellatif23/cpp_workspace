9215dbe64a0ce388f51740165e6db97451a63d16
Ahmed Abdellatif <s270144@studenti.polito.it>
2023-07-05 11:12:51 +0200

    Accessing class members
    using dot and arrow operator

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
-// Declare Classes and Objects
+// Accessing class members 
+
+/*
+if you have an object using dot operator
+refObject.refAttribute
+refObject.refMethod(pass the argument)
+
+if you have a pointer to the object 
+dereference the pointer then use dot operator 
+or use the arrow operator
+Note : parenthesis is a must 
+
+(*refObject).refAttribute 
+refObject->refAttribute 
+
+(*refObject).refMethod(pass the argument)
+refObject->refMethod(pass the argument)
+
+same as in C for structures and unions 
+*/
 
 #include <iostream>
 #include <string>
@@ -9,45 +28,68 @@ using namespace std;
 
 // Minimal player class 
 class Player {
+public:
 	// attributes : to be initialized using constructors 
 	string name;
 	int health;
 	int xp;
 	// methods
-	void talk(string);  // functions prototypes 
+	void talk(string textToSay) {cout << name << " says " << textToSay << endl;}  // functions prototypes 
 	bool is_dead();		
 };
 
-
-class Account {
+class Account
+{
+public:
 	// attributes 
 	string name;
 	double balance;
+
 	// methods 
-	bool deposit(double);
-	bool withdraw(double);
+	bool deposit(double bal) {balance += bal; cout << "in deposit" << endl; return 0;} // don't forget return types
+	bool withdraw(double bal) {balance -= bal; cout << "in withdraw" << endl; return 0;}
 };
 
 
+
+
+
 int main () {
-	Player frank;	// instaces 
-	Player hero;
+	Player ahmed;	// instaces 
+	
+	Account ahmedAccount;
 
-	Account frankAccount;
-	Account heroAccount;
-	// array 
-	Player players[] {frank,hero};
 
-	// create a vector 
+	// accessing attributes of the class 
+	ahmed.name = "ahmed";
+	ahmed.health = 200;
+	ahmed.xp = 12;
+	ahmed.talk("ciao dude");
 
-	vector<Player> player_vec {frank};
-	player_vec.push_back(hero); // that should add hero to that vector 
+	ahmedAccount.name = "Ahmed's Account";
+	ahmedAccount.balance = 200;
+	ahmedAccount.deposit(100);
+	ahmedAccount.withdraw(22);
 
 
 	// create objects on the heap 
 
 	Player *enemy {nullptr};
 	enemy = new Player;
+
+	enemy->name = "none";
+	enemy->health = 200;
+	(*enemy).xp = 11;
+	enemy->talk("dude i will kill you !");
 	delete enemy;
+
+
+	Account *imeneAccount {nullptr};
+	imeneAccount = new Account;
+	imeneAccount->balance = 200;
+	imeneAccount->name = "imene account";
+	imeneAccount->deposit(233);
+	imeneAccount->withdraw(110);
+
 	return 0; 
 }
