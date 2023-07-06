4798c24d8bc396baa257d01004a3812ab0a6c46d
Ahmed Abdellatif <s270144@studenti.polito.it>
2023-07-06 14:11:46 +0200

    Separating specification from implementation

--- header_file.h
+++ header_file.h
@@ -1,6 +1,18 @@
+// include guard
 #ifndef HEADER_FILE_H_
 #define HEADER_FILE_H_
 
 #define TMP (1U)
+
+class Account
+{
+private:
+    double balance;
+public:
+    void set_balance(double bal);
+    double get_balance();
+};
+
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-// Class member access modifiers
+// Separating specification from implementation
 
 /*
-public
-
-private : if you try to access private memebrs you get complier error
-private is accessible within a class or by its friends
+	Methods are implemented in cpp file but it is declared in .h
+	either using include guards or #pragma once
 */
 
 #include <iostream>
@@ -14,37 +12,18 @@ private is accessible within a class or by its friends
 
 using namespace std;
 
-// Minimal player class 
-class Player {
-private:
-	// attributes : to be initialized using constructors 
-	string name {"Ahmed"};
-	int health;
-	int xp;
-public:
-	// methods
-	void talk(string textToSay) {cout << name << " says " << textToSay << endl;}  // functions prototypes 
-	bool is_dead();		
-};
+void Account::set_balance(double bal) {
+	balance = bal;
+}
 
-class Account
-{
-public:
-	// attributes 
-	string name;
-	double balance;
-
-	// methods 
-	bool deposit(double bal) {balance += bal; cout << "in deposit" << endl; return 0;} // don't forget return types
-	bool withdraw(double bal) {balance -= bal; cout << "in withdraw" << endl; return 0;}
-};
-
-
-
+double Account::get_balance() {
+	return balance;
+}
 
 
 int main () {
-	Player ahmed;
-	ahmed.talk("hello");
+	Account ahmedAccount;
+	ahmedAccount.set_balance(100);
+
 	return 0; 
 }
