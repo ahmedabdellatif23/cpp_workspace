bf7fd803712960d62d67313843f3bbd3c9335722
Ahmed Abdellatif <s270144@studenti.polito.it>
2023-07-10 10:23:49 +0200

    Move constructor
    Move constructor moves an object rather than copy it, uses r-values reference operator (&&)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
-// Shallow copy
+// Move constructor
 
 /*
-	when we have a raw pointer as a class member,
-	shollow copy - only the pointer is copied not what it points to
-	problem : source and the newly created object both point to the same data area!
+	sometimes when we execute code the compiler creates unnamed temp values
+	As copy constructors doing deep copying can have significant performance bottleneck
+	Move constructor moves an object rather than copy it, uses r-values reference operator (&&)
+	optional but not recommended
+
+	 int x {100}
+	 void func(int &num)  func that expects an l value ref
+	 func(x) ok ! since x is an lvalue
+	 func(200); nok ! since 200 is an r-value
+
+	 however
+	 void func(int &&num) func that expects an r value ref
+	 func(200) ok !
+	 func(x) nok !
+
+	instead of making a deep copy of the move constructor
+	'moves' the resources
+	simply copies the address of the resources from source to current object
+	and nulls out the pointer in the source pointer
+
+	syntax -r-value reference
+	Type::Type(Type &&source)     no const qualifier for source, par is r-value ref.
 */
 
 #include <iostream>
@@ -13,50 +32,61 @@
 
 using namespace std;
 
-class Deep
+class Move
 {
 private:
-	int *data;
-
+	int *data; // raw pointer
 public:
 	void set_data_value(int d) { *data = d; }
 	int get_data_value() { return *data; }
-	/* overloaded contructors */
-	Deep(int d);
-	/* copy constructor */
-	Deep(const Deep &source);
-	/* destructor */
-	~Deep();
+	Move(int d);			  // constructor
+	// Move(const Move &source); // copy constructor
+	Move(Move &&source);	  // move constructor
+	~Move();				  // destructor
 };
 
-Deep::Deep(int d)
+Move::Move(int d)
 {
-	data = new int; // allocate storage for an int
-	*data = d;
-}
-/* implementation of copy constructor : changes here to overcome shallow  */
-Deep::Deep(const Deep &source) : Deep {*source.data}
-{
-	/* Create a new storage and copy values */
-	// data = new int; // allocate memory for int
-	// *data = *source.data;
-	// cout << "deep copy" << endl;
-}
-Deep::~Deep()
-{
-	delete data;
-	cout << "free data" << endl;
+	data = new int; // allocate storage
+	*data = d;		// copy data
+	cout << "constructor for: " << d << endl;
 }
 
-void display_deep(Deep s) {
-	cout << s.get_data_value() << endl;
+// Move::Move(const Move &source)
+// 	: Move{*source.data}
+// {
+
+// 	cout << "copy constructor is being called: " << *data << endl;
+// }
+
+Move::Move(Move &&source)
+	: data{source.data}
+{ // not a deep copy
+	/* steal the data then null out the source pointer */
+	source.data = nullptr;
+	cout << "move constructor is being called: " << *data << endl;
+}
+
+Move::~Move()
+{
+	if (data != nullptr)
+	{
+		cout << "destructor is being called : " << *data << endl;
+	}
+	else
+	{
+		cout << "destructor is being called for nullptr " << endl;
+	}
+	delete data;
 }
 
 int main()
 {
-	Deep obj1{100};
-	display_deep(obj1);
-	Deep obj2{obj1};
-	obj2.set_data_value(240);
+	vector<Move> vec;
+	vec.push_back(Move{10});
+	vec.push_back(Move{30});
+	vec.push_back(Move{34});
+	vec.push_back(Move{3330});
+	vec.push_back(Move{301});
 	return 0;
 }
