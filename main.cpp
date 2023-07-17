aea16b6709577b0ce5a1f20633e34a7f5a2c536e
Ahmed Abdellatif <s270144@studenti.polito.it>
2023-07-17 09:56:34 +0200

    struct vs classes
    	structs are the same as classes except that
    	struct members are public by default
    	classes members are private by default
    	use structs for passive objects with public access
    	use classes for active objects with private access
    	implement getters and setters as needed
    	implement member methods as needed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-// Static class members
+// Structs Vs Classes
 
 /*
-	Class data members can be declared as static
-	single data member that belongs to the class not the objects
-	useful to store class wide information
-
-	Static functions have access to ONLY static variables
+	structs are the same as classes except that
+	struct members are public by default
+	classes members are private by default
+	use structs for passive objects with public access
+	use classes for active objects with private access
+	implement getters and setters as needed
+	implement member methods as needed
 */
 
 #include <iostream>
@@ -13,45 +15,23 @@
 #include <vector>
 #include "header_file.h"
 
-using namespace std;
-/* static variables must be initialized */
+// class Person
+// {
+// 	std::string name;
+// 	std::string get_name() {return name;};
+// };
 
-int Player::num_players{0};
-/* implements the constructor */
-Player::Player(std::string name_val, int health_val, int xp_val)
-	: name{name_val}, health{health_val}, xp{xp_val}
+struct Person
 {
-	++num_players;
-}
+	std::string name;
+	std::string get_name() {return name;};
+};
 
-Player::~Player()
-{
-	--num_players;
-}
 
-int Player::get_num_players()
+main()
 {
-	return num_players;
-}
-
-void display_active_players()
-{
-	cout << "Active players :" << Player::get_num_players() << endl;
-}
-
-int main()
-{
-	display_active_players();
-	Player Hero{"Hero"};
-	display_active_players();
-	{
-		Player Ahmed{"Ahmed"};
-		display_active_players();
-	} // after this block the object Ahmed is destroyed so destructor will be called and --num
-	display_active_players();
-	Player *ahmedDynamic = new Player("ahmedDynamic",100,20);
-	display_active_players();
-	delete ahmedDynamic;
-	display_active_players();
+	Person p;
+	p.name = "Ahmed";
+	std::cout << p.get_name();
 	return 0;
 }
