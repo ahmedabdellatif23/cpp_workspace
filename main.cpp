b4769e018c53397677a5975db04fe9da1ec259b1
Ahmed Abdellatif <57152328+ahmedabdellatif23@users.noreply.github.com>
2023-12-28 18:15:58 +0200

    functions overloading

--- main.cpp
+++ main.cpp
@@ -1,18 +1,45 @@
-/* Section 9 */
+/* Section 11 */
+// functions overloading (using the same name)
 
 #include <iostream>
+#include <string>
 #include <vector>
-
 
 using namespace std;
 
+void print(int);
+void print(double);
+void print(string, string);
+void print(vector<int>);
+
+void print(int num)
+{
+	cout << "Printing int: " << num << endl;
+}
+
+void print(double num)
+{
+	cout << "Printing double: " << num << endl;
+}
+
+void print(vector<int> v)
+{
+	cout << "Printing vector of strings : ";
+
+	for(auto s:v)
+	{
+		cout << s;
+		cout << endl;
+	}
+
+}
 
 int main()
 {
-	int num;
-	num = 12;
+	print(123.5);
+	
+	vector<int> three_stooges {10,20};
 
-	cout << num << "is " << ((num%2 == 0) ? " even" : " odd") << endl;
-
+	print(three_stooges); // 
 	return 0;
 }
