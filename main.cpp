d01cf036aa32674297c7545aaae9781ca399b60e
AV\ahmedabdellatif <ahmedabdellatif@av.it.pt>
2024-03-04 13:41:43 +0000

    pointers minor changes

--- main.cpp
+++ main.cpp
@@ -21,18 +21,14 @@ public:
 
 main()
 {
-	// Test t1{1000};  // both construtor and destructors are called because t1 is on stack
-	// Test *t2 = new Test{2000}; // destructor is never called for this
-	// delete t2;
+	int *p1 {nullptr};
+	long long *p2 {nullptr};
+	vector<string> *p3 {nullptr};
+	int score {10};
+	double high_temp {100.3};
 
-	std::unique_ptr<Test> t1{new Test{100}};				 // using uniqe pointer, t1 is unique pointer to Test object and we are initializing the object to 100
-	std::unique_ptr<Test> t2 = std::make_unique<Test>(1000); //
+	int *score_ptr {nullptr};
+	score_ptr = &score;
 
-	std::unique_ptr<Test> t3;
-	t3 = std::move(t1); // moving ownership from t1 to t3, now t1 becomes pointing to no where.
-	if (!t1)
-	{
-		std::cout << "t1 is nullptr" << std::endl;
-	}
 	return 0;
 }
